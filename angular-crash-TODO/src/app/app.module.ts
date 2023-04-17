eb0f9fde052d2d3d31ec4aa9543fa785e46a738b
abhinav-kesari <60466018+abhinav-kesari@users.noreply.github.com>
2023-04-17 11:20:23 +0530

    Add files via upload

---
+++ angular-crash-TODO/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { TaskComponent } from './components/task/task.component';
+import { TaskItemComponent } from './components/task-item/task-item.component';
+import { AddTasksComponent } from './components/add-tasks/add-tasks.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ButtonComponent,
+    TaskComponent,
+    TaskItemComponent,
+    AddTasksComponent,
+    AboutComponent,
+    FooterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
